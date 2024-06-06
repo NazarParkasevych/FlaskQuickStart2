99812f74042177b2626012cc2406c1c970cbacb3
Parkasevych Nazar <parkasevychnazarij@gmail.com>
2024-06-06 12:59:12 +0300

    Created site

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+users = {
+    'Nazar': '123456',
+    'Yura': '1234567',
+}
 
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
+
+@app.route("/bye")
+def bye_world():
+    return render_template('bye.html')
+
+
+@app.route("/hello/<username>")
+def hello_user(username):
+    return render_template(
+        'hello_user.html',
+        username=username
+    )
+
+
 @app.route("/users")
 def user_list():
-    users_list = [
-        'Parkasevych Nazar'
-    ]
+    users_list = list(users.keys())
 
     return render_template(
         'user_list.html',
@@ -19,15 +37,42 @@ def user_list():
     )
 
 
-@app.route("/users/string:username>")
+@app.route("/users/<string:username>")
 def user(username):
+    if 'username' in session and session['username'] == username:
+        return render_template(
+            'user.html',
+            username=username
+        )
+
+
+@app.route("/posts")
+def posts():
     return render_template(
-        'user.html',
-        username=username,
+        'posts.html',
+        posts=range(1, 11)
     )
 
-@app.route('/login')
+
+@app.route("/posts/<int:post_id>")
+def show_post(post_id):
+    return render_template(
+        'show_post.html',
+        post_id=post_id
+    )
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users and users[username] == password:
+            session['username'] = username
+
+            return redirect(url_for('user', username=username))
+
     return render_template('login.html')
 
 
@@ -39,3 +84,7 @@ def register():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=0000)
