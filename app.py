62f1fd543c415524199a515664cb608c6a3751f9
Parkasevych Nazar <parkasevychnazarij@gmail.com>
2024-06-06 14:54:10 +0300

    Created site

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
@@ -70,10 +70,18 @@ def login():
 
         if username in users and users[username] == password:
             session['username'] = username
+            flash('Ви Зареєструвались. Дякую')
 
             return redirect(url_for('user', username=username))
 
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    username = session.pop('username', None )
+    if username:
+        flash('Ви нас кинули')
+    return redirect(url_for('home'))
 
 
 @app.route('/register')
